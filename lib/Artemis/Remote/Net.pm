a7394ea97b2c2b3df6818f7475137ab3ddae9c8d
Maik Hentsche <maik.hentsche@amd.com>
2010-04-08 16:05:24 +0200

    Remote: added common net functions

---
+++ lib/Artemis/Remote/Net.pm
@@ -0,0 +1,74 @@
+package Artemis::Remote::Net;
+
+use strict;
+use warnings;
+
+use Moose;
+
+=head1 NAME
+
+Artemis::Remote::Net - Communication with MCP
+
+=head1 SYNOPSIS
+
+ use Artemis::Remote::Net;
+
+=head1 FUNCTIONS
+
+=head2 mcp_send
+
+Tell the MCP server our current status. This is done using a TCP connection.
+
+@param string - message to send to MCP
+
+@return success - 0
+@return error   - error string
+
+=cut
+
+sub mcp_send
+{
+        my ($self, $message) = @_;
+        my $server = $self->cfg->{mcp_host} or return "MCP host unknown";
+        my $port   = $self->cfg->{mcp_port} || 7357;
+
+        my $yaml = Dump($message);
+	if (my $sock = IO::Socket::INET->new(PeerAddr => $server,
+					     PeerPort => $port,
+					     Proto    => 'tcp')){
+		print $sock ("$yaml");
+		close $sock;
+	} else {
+                return("Can't connect to MCP: $!");
+	}
+        return(0);
+}
+
+
+
+1;
+
+=head1 AUTHOR
+
+OSRC SysInt Team, C<< <osrc-sysint at elbe.amd.com> >>
+
+=head1 BUGS
+
+None.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+ perldoc Artemis
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2008 OSRC SysInt Team, all rights reserved.
+
+This program is released under the following license: restrictive
+
